7ad8fb090491819882905833e2ec40f3f5c65cf0
seungmai <76721552+seungmai@users.noreply.github.com>
2021-02-06 20:48:58 +0900

    JS: 실습연습

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const navbar = document.querySelector('#navbar');
 document.addEventListener('click', (e) => {
     const target = target.link('.home');
 });
+
+const section = document.querySelector('#section');
+document.addEventListener('scroll', () => {
+    console.log(section);
+})
